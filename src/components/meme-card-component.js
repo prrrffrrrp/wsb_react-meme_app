6160501e67290b3fe1a971020e75b9a2e7a715dc
arnau <arnau.vidal@gigaset.com>
2021-05-10 07:25:42 +0200

    Add upvote and Downvote buttons

--- src/components/meme-card-component.js
+++ src/components/meme-card-component.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 
 export default function MemeCard(props) {
+    const [upvotes, setUpvotes] = useState(props.meme.upvotes);
+    const [downvotes, setDownvotes] = useState(props.meme.downvotes);
+
+    const clickUpvotes = () => {
+        setUpvotes(upvotes + 1);
+    }
+
+    const clickDownvotes = () => {
+        setDownvotes(downvotes + 1);
+    }
 
     return  <section>   
-                 <h4>{props.meme.title}</h4>
-                 <img src={props.meme.img} alt="thug virus"/>
-                 <p>Votes {Number(props.meme.upvotes - props.meme.downvotes)}</p>
-             </section>   
+                <h4>{props.meme.title}</h4>
+                <img src={props.meme.img} alt="thug virus"/>
+                <p>Votes {upvotes - downvotes}</p>
+                <button onClick={clickUpvotes}  class="btn btn-primary"> + </button> 
+                <button onClick={clickDownvotes} class="btn btn-secondary"> - </button>
+            </section>
 }
