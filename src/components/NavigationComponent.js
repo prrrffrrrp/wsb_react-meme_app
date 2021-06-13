19f05c9c06a9f43967f7b488bce673208015d9b7
arnau <arnau.vidal@gigaset.com>
2021-06-13 18:09:58 +0200

    Make the app mobile friendly (responsive design)

--- src/components/NavigationComponent.js
+++ src/components/NavigationComponent.js
@@ -6,20 +6,13 @@ export default function Navigation() {
   return (
     <div>
       <Navbar className="py-0" bg="light" expand="lg" sticky="top">
-        <NavLink className="navbar-brand mx-auto" to="/home"><h1>memes_wtf</h1></NavLink>
-      </Navbar>
-
-      <Navbar className="py-0" bg="light" expand="lg" sticky="top">
+        <NavLink className="navbar-brand mr-auto" to="/home"><h1>memes_wtf</h1></NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-
-          <Nav className="mx-auto nav-container py-0">
+          <Nav className="ml-auto nav-container py-0">
             <NavLink className="nav-link navbar-item py-0" to="/regular">regular</NavLink>
             <NavLink className="nav-link navbar-item py-0" to="/hot">hot</NavLink>
-          </Nav>
-
-          <Nav className="mx-auto py-0">
-            <NavLink className="nav-link navbar-item py-0" to="/upload-meme">upload meme</NavLink>
+            <NavLink className="nav-link navbar-item py-0" to="/upload-meme">upload_meme</NavLink>
           </Nav>
 
         </Navbar.Collapse>
